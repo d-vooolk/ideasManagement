43a3de49960a1302ff01d3259e27bacece6ad886
d-vooolk <volk.dmitrii@otr.ru>
2025-04-22 02:30:42 +0300

    add monorepa scripts

--- webapp/src/pages/AllIdeasPage/index.tsx
+++ webapp/src/pages/AllIdeasPage/index.tsx
@@ -1,29 +1,25 @@
 import { trpc } from '../../lib/trpc.tsx';
 
 export const Index = () => {
-    const {
-        data, error, isLoading, isFetching, isError
-    } = trpc.getIdeas.useQuery();
+  const { data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery();
 
-    if (isLoading || isFetching) {
-        return <span>Loading ...</span>
-    }
+  if (isLoading || isFetching) {
+    return <span>Loading ...</span>;
+  }
 
-    if (isError) {
-        return <span>Error: { error.message }</span>
-    }
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
 
-    return (
-        <div>
-            <h1>Ideas</h1>
-            {
-                data?.ideas?.map(idea => (
-                    <div key={idea.nick}>
-                        <h2>{idea.name}</h2>
-                        <p>{idea.description}</p>
-                    </div>
-                ))
-            }
+  return (
+    <div>
+      <h1>Ideas</h1>
+      {data?.ideas?.map((idea) => (
+        <div key={idea.nick}>
+          <h2>{idea.name}</h2>
+          <p>{idea.description}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
